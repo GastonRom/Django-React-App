2c604dc394e13207c0c691aa39bcc0219e4baf5f
Gastón Romero <gastiromero5@gmail.com>
2025-02-22 18:49:30 -0300

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,11 @@
 import axios from "axios"
 import { ACCESS_TOKEN } from "./constants"
 
+const apiUrl = "https://aa8f5973-f3d4-44af-b7d6-8c8c24bc707d-dev.e1-us-east-azure.choreoapis.dev/djangoreactproject/backend/v1"
+
 // importo cualquier cosa que este dentro de un archivo de variable de entorno
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 // me fijo en mi localstorage si tengo un access token, si es asi lo agrego como authorization header al request
